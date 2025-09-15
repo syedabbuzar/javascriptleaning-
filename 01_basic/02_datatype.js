b300115b164d94e2a0cc8ccc1e862e80642f4a48
Syed Abbuzar <162713688+syedabbuzar@users.noreply.github.com>
2025-09-15 20:11:11 +0000

    datatypes

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,19 @@
+"use strict"; // Treat all ja code as newer version 
+
+//alert(3 +3) // we are using node.js , not browser 
+
+console.log(3+3);
+//code readability should be high 
+
+console.log("hello faizuu");
+
+let name = "Syed"
+let age = 19
+let isLoggedIn = false 
+
+//number => 2 to power of 53
+//bigint 
+ //string => ""
+ //Boolean => ture/false
+ //null 
+ //undefined =>
--- 01_basic/variable.js
+++ 01_basic/variable.js
@@ -1,4 +1,17 @@
-const accoundId = 0001
+const accoundId = 324
 let accountEmail = "syedabbuzar777@g,ail.com"
 var accountPassword = "777000"
 accountCity = "Nanded "
+
+//accoundId = 2 //not allowed to change 
+ 
+accountEmail = "faizuuu111@gmail.com"
+accountPassword = "123456"
+accountCity = " =mumbai"
+let accountState;
+
+/* prefer not use var 
+because of issue in block scope and functional scope */ 
+
+console.log("accoundId");
+console.table([accoundId, accountEmail,accountPassword,accountCity])
